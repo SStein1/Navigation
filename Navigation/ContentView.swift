409203b63d2668db441e933d546e4f17e62bccf4
Shira Stein <sstein@jkha.org>
2025-04-17 13:57:19 -0400

    Added another view

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            VStack {
+            VStack (spacing: 20){
                 Text("This is the root view")
-                NavigationLink(destination: Text ("You've arrived to the second view!")) {
-                    Text ("Click Me!")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.blue)
+                    
+                NavigationLink(destination: Text ("You've arrived to the second view!")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.orange)) {
+                        Text ("Click Me!")
+                            .foregroundColor(Color.purple)
+                    }
+                        NavigationLink(destination: Text ("This is another view! :)")
+                            .font(.headline)
+                            .foregroundColor(Color.pink)) {
+                        Text ("Tap for more!")
+                            .foregroundColor(Color.green)
+                    
                 }
             }
         }
