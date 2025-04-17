718754972050788481d7464a72fa7e702d447f9a
Shira Stein <sstein@jkha.org>
2025-04-17 13:28:58 -0400

    Initial Commit

---
+++ NavigationTests/NavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTests.swift
+//  NavigationTests
+//
+//  Created by Shira Stein on 4/17/25.
+//
+
+import Testing
+@testable import Navigation
+
+struct NavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
